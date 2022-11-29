c04b014c1723c46aab57018e1c57c3681fe5056f
Renanbfreitas <renanbf1992@outlook.com>
2022-11-29 18:14:40 -0300

    Completa requisitos 4 e 6 de testes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
+describe('Testando o arquivo App', () => {
+  test('1- Verifica se existem campos para digitar email e senha', () => {
+    render(<App />);
+    const email = screen.getByTestId('email-input');
+    const senha = screen.getByTestId('password-input');
+    expect(email).toBeDefined();
+    expect(senha).toBeDefined();
+  });
+});
+
+test('2- Verifica se a página muda após o usuário clicar no botão de Enter', () => {
   render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+  const email = screen.getByTestId('email-input');
+  const senha = screen.getByTestId('password-input');
+  const enter = screen.getByTestId('login-submit-btn');
+  userEvent.type(email, 'email@test.com');
+  userEvent.type(senha, '1234567');
+  userEvent.click(enter);
 });
