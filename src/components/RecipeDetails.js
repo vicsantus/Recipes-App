26588c4a6cdd62e96ccacdd9f67b0d108d944a44
victrosantos <victor.santos.fk@hotmail.com>
2022-12-06 20:54:38 -0300

    Feitos req do 37 ao 43 faltando testes

--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -1,83 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import clipboardCopy from 'clipboard-copy';
-import recommendationFetch from '../services/fetchRecom';
-import favoriteToStorage from '../services/favoriteToStorage';
-import shareIcon from '../images/shareIcon.svg';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
-import whiteHaertIcon from '../images/whiteHeartIcon.svg';
 import '../App.css';
+// import ApiContext from '../context/ApiContext';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHaertIcon from '../images/whiteHeartIcon.svg';
+import favoriteToStorage from '../services/favoriteToStorage';
+import useApiResponse from './hooks/recipeDetails/useApiResponse';
+import useCopieFav from './hooks/recipeDetails/useCopieFav';
 
 const six = 6;
-const fiveSeconds = 5000;
 
 function RecipeDetails() {
   const history = useHistory();
-  const [apiResponse, setApiResponse] = useState(null);
-  const [ingreds, setIngreds] = useState([]);
-  const [measure, setMeasure] = useState([]);
   const { location: { pathname } } = history;
   const mOrD = pathname.includes('/meals/');
   const idPath = JSON.parse(pathname
     .replace(mOrD ? '/meals/' : '/drinks/', ''));
-
-  const [youTubeId, setYouTubeId] = useState(null);
-  const [recommendation, setRecommendation] = useState([]);
-  const [nameToMap, setNameToMap] = useState('');
-  const [copied, setCopied] = useState(false);
-  const [favorite, setFavorite] = useState();
-
-  useEffect(() => {
-    const namePath = pathname.split('/');
-    recommendationFetch(namePath[1]).then((res) => setRecommendation(res));
-    if (namePath[1] === 'meals') {
-      setNameToMap('Drink');
-    }
-    if (namePath[1] === 'drinks') {
-      setNameToMap('Meal');
-    }
-  }, [pathname, apiResponse]);
-
-  useEffect(() => {
-    const makeFetch = async () => {
-      let url = '';
-      if (mOrD) {
-        url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idPath}`;
-      } else {
-        url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idPath}`;
-      }
-      const fetched = await fetch(url);
-      const json = await fetched.json();
-      setApiResponse(mOrD ? json.meals : json.drinks);
-    };
-    makeFetch();
-  }, [idPath, pathname, mOrD]);
-
-  useEffect(() => {
-    if (apiResponse) {
-      const ingre = Object
-        .keys(apiResponse[0])
-        .map((item) => (item.includes('strIngredient') ? item : null));
-      const ingreWithOutNull = ingre.filter((item) => item !== null);
-      const ingreExist = ingreWithOutNull?.map((igd) => apiResponse[0][igd]);
-      const ingreExistNotNull = ingreExist
-        .filter((item) => (item !== null) && (item !== ''));
-
-      const measu = Object
-        .keys(apiResponse[0])
-        .map((item) => (item.includes('strMeasure') ? item : null));
-      const measuWithOutNull = measu.filter((item) => item !== null);
-      const measuExist = measuWithOutNull?.map((igd) => apiResponse[0][igd]);
-      const measuExistNotNull = measuExist.filter((item) => item !== null);
-      setIngreds(ingreExistNotNull);
-      setMeasure(measuExistNotNull);
-
-      if (mOrD) {
-        const a = apiResponse[0].strYoutube.split('=')[1];
-        setYouTubeId(a);
-      }
-    }
-  }, [apiResponse, mOrD]);
+  // const states = useContext(ApiContext);
+  const {
+    apiResponse,
+    ingreds,
+    measure,
+    youTubeId,
+    nameToMap,
+    recommendation,
+  } = useApiResponse({ idPath, pathname, mOrD });
+  const {
+    copied,
+    favorite,
+    setFavorite,
+    shareRecipe,
+  } = useCopieFav({ pathname });
 
   useEffect(() => {
     if (apiResponse !== null) {
@@ -95,13 +49,40 @@ function RecipeDetails() {
         setFavorite(verify);
       }
     }
-  }, [apiResponse, pathname]);
+  }, [apiResponse, pathname, setFavorite]);
 
-  const shareRecipe = async () => {
-    setCopied(true);
-    setTimeout(() => setCopied(false), fiveSeconds);
-    await clipboardCopy(`http://localhost:3000${pathname}`);
-  };
+  // useEffect(() => {
+  //   if (apiResponse && ingreds.length > 0 && measure.length > 0 && !states.recipe) {
+  //     states.setRecipe({
+  //       apiResponse,
+  //       ingreds,
+  //       measure,
+  //       mOrD,
+  //       youTubeId,
+  //       nameToMap,
+  //       pathname,
+  //       favorite,
+  //       setFavorite,
+  //     });
+  //   }
+  //   if (states.recipe !== null && states.recipe?.favorite !== favorite) {
+  //     states.setRecipe({
+  //       ...states.recipe,
+  //       favorite,
+  //     });
+  //   }
+
+  //   console.log(states.recipe);
+  // }, [apiResponse,
+  //   ingreds,
+  //   measure,
+  //   mOrD,
+  //   youTubeId,
+  //   nameToMap,
+  //   pathname,
+  //   favorite,
+  //   setFavorite,
+  //   states]);
 
   return (
     <div>
@@ -147,7 +128,7 @@ function RecipeDetails() {
         <button
           type="button"
           data-testid="share-btn"
-          onClick={ () => shareRecipe() }
+          onClick={ shareRecipe }
         >
           <img src={ shareIcon } alt="alt" />
         </button>
--- src/components/RecipeInProgress.js
+++ src/components/RecipeInProgress.js
@@ -1,8 +1,201 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+// import ApiContext from '../context/ApiContext';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHaertIcon from '../images/whiteHeartIcon.svg';
+import favoriteToStorage from '../services/favoriteToStorage';
+import useApiResponse from './hooks/recipeDetails/useApiResponse';
+import useCopieFav from './hooks/recipeDetails/useCopieFav';
+// import useApiResponse from './hooks/recipeDetails/useApiResponse';
 
 function RecipeInProgress() {
+  // const states = useContext(ApiContext);
+
+  const history = useHistory();
+  const { location: { pathname } } = history;
+  const mOrD = pathname.includes('/meals/');
+  const idPath = JSON.parse(pathname
+    .replace('/in-progress', '')
+    .replace(mOrD ? '/meals/' : '/drinks/', ''));
+  const pathWithOutProg = pathname.replace('/in-progress', '');
+  const [ingredCheck, setIngredCheck] = useState([]);
+  const {
+    apiResponse,
+    ingreds,
+    measure,
+    youTubeId,
+    nameToMap,
+  } = useApiResponse({ idPath, pathname, mOrD });
+  const btnOk = ingreds.length === ingredCheck.length;
+  // const {
+  //   favorite,
+  //   setFavorite,
+  // } = states.recipe;
+  const {
+    copied,
+    favorite,
+    setFavorite,
+    shareRecipe,
+  } = useCopieFav({ pathname: pathWithOutProg });
+
+  const onChecked = ({ target, idxx }) => {
+    if (target.checked) {
+      setIngredCheck([
+        ...ingredCheck,
+        idxx,
+      ]);
+    } else {
+      const ingWithOutThis = ingredCheck.filter((idx) => idx !== idxx);
+      setIngredCheck(ingWithOutThis);
+    }
+  };
+
+  useEffect(() => {
+    const local = JSON.parse(localStorage.getItem('inProgressRecipes'));
+    if (!local || local.length === 0) {
+      localStorage.setItem('inProgressRecipes', JSON.stringify(ingredCheck));
+    } else if (local !== ingredCheck && ingredCheck.length !== 0) {
+      localStorage.setItem('inProgressRecipes', JSON.stringify(ingredCheck));
+    } else {
+      setIngredCheck(local);
+    }
+  }, [ingredCheck]);
+
+  useEffect(() => {
+    const local = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    const idsLocal = local?.map((objs) => objs.id);
+    if (idsLocal?.includes(JSON.stringify(idPath))) {
+      setFavorite(true);
+    }
+  }, [apiResponse, idPath, setFavorite]);
+  console.log(apiResponse);
+
+  const clickFinish = () => {
+    const local = JSON.parse(localStorage.getItem('doneRecipes'));
+    const date = new Date();
+    const mgxMenosUm = -1;
+    const mgxTres = 3;
+    const extract = (datee) => datee
+      .toISOString()
+      .split(/[^0-9]/)
+      .slice(0, mgxMenosUm)
+      .filter((dt, idx) => (idx === mgxTres ? `${JSON.parse(dt) - 2}` : dt));
+    const newDate = extract(date);
+    console.log(newDate);
+    const doneRec = {
+      id: mOrD ? apiResponse[0].idMeal : apiResponse[0].idDrink,
+      type: mOrD ? 'meal' : 'drink',
+      nationality: apiResponse[0]?.strArea !== null ? apiResponse[0]?.strArea : '',
+      category: apiResponse[0]?.strCategory !== null ? apiResponse[0]?.strCategory : '',
+      alcoholicOrNot: !mOrD ? apiResponse[0]?.strAlcoholic : '',
+      name: mOrD ? apiResponse[0]?.strMeal : apiResponse[0]?.strDrink,
+      image: mOrD ? apiResponse[0]?.strMealThumb : apiResponse[0]?.strDrinkThumb,
+      doneDate: newDate,
+      tags: apiResponse[0]?.strTags,
+    };
+    if (!local || local.length === 0) {
+      localStorage.setItem('doneRecipes', JSON.stringify([doneRec]));
+    } else if (!local.includes(doneRec)) {
+      localStorage.setItem('doneRecipes', JSON.stringify([...local, doneRec]));
+    }
+    localStorage.setItem('inProgressRecipes', JSON.stringify([]));
+  };
+
   return (
-    <div>RecipeInProgress</div>
+    <div>
+      {apiResponse?.map((food, idx) => (
+        <section key={ idx }>
+          <img
+            style={ { width: '175px' } }
+            data-testid="recipe-photo"
+            src={ mOrD ? food.strMealThumb : food.strDrinkThumb }
+            alt={ mOrD ? food.strMealThumb : food.strDrinkThumb }
+          />
+          <h1
+            data-testid="recipe-title"
+          >
+            {mOrD ? food.strMeal : food.strDrink }
+          </h1>
+          <h2
+            data-testid="recipe-category"
+          >
+            {mOrD ? food.strCategory : (`${food.strCategory} ${food.strAlcoholic}`)}
+          </h2>
+          {!mOrD && (
+            <h2>{food.strAlcoholic}</h2>
+          )}
+          Ingredientes
+          <aside>
+            {ingreds?.map((ingred, idxx) => (
+              <label
+                data-testid={ `${idxx}-ingredient-step` }
+                key={ idxx }
+                htmlFor={ `ingredient ${idxx + 1}` }
+                onChange={ ({ target }) => onChecked({ target, idxx }) }
+                style={ ingredCheck?.includes(idxx)
+                  ? { textDecoration: 'line-through solid rgb(0, 0, 0)' }
+                  : { textDecoration: 'none solid rgb(0, 0, 0)' } }
+              >
+                <input
+                  id={ `ingredient ${idxx + 1}` }
+                  checked={ !!ingredCheck?.includes(idxx) }
+                  type="checkbox"
+                />
+                {`${measure[idxx]} ${ingred}`}
+              </label>
+            ))}
+          </aside>
+          <p data-testid="instructions">{food.strInstructions}</p>
+          {mOrD && (
+            <iframe
+              src={ `https://www.youtube.com/embed/${youTubeId}` }
+              title="YouTube video player"
+              data-testid="video"
+            />
+          )}
+        </section>
+      ))}
+      <div>
+        <button
+          type="button"
+          data-testid="share-btn"
+          onClick={ shareRecipe }
+        >
+          <img src={ shareIcon } alt="alt" />
+        </button>
+
+        <button
+          type="button"
+          onClick={ () => {
+            favoriteToStorage(apiResponse[0], nameToMap);
+            setFavorite(!favorite);
+          } }
+        >
+          <img
+            src={ !favorite ? whiteHaertIcon : blackHeartIcon }
+            alt="favorite icon"
+            data-testid="favorite-btn"
+          />
+
+        </button>
+        {copied && (<span>Link copied!</span>)}
+      </div>
+      <div>
+        <button
+          className="btnFinishCont"
+          type="button"
+          data-testid="finish-recipe-btn"
+          onClick={ () => {
+            clickFinish();
+            history.push('/done-recipes');
+          } }
+          disabled={ !btnOk }
+        >
+          Finish Recipe
+        </button>
+      </div>
+    </div>
   );
 }
 
