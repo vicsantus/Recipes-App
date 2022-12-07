04bde4ef200178052bcb829b426751ac27d922ee
Tarcisio R Sacramento <tarcisacra333@gmail.com>
2022-12-06 20:14:17 -0400

    Commit Done Page Principal, anterior errado sry

--- src/components/DoneRecipes.js
+++ src/components/DoneRecipes.js
@@ -1,58 +1,179 @@
 import React, { useState, useEffect } from 'react';
+import clipboardCopy from 'clipboard-copy';
+import { useHistory } from 'react-router-dom';
 import Header from './Header';
+import shareIcon from '../images/shareIcon.svg';
 
+const fiveSeconds = 5000;
 function DoneRecipes() {
+  const history = useHistory();
+  const { location: { pathname } } = history;
+
   const [doneRecipes, setDoneRecipes] = useState([]);
+  const [doneRecipesFilter, setDoneRecipesFilter] = useState([]);
+  const [doneRecipesShow, setDoneRecipesShow] = useState([]);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     const doneRecipesInStorage = JSON.parse(localStorage.getItem('doneRecipes')) || [];
     setDoneRecipes(doneRecipesInStorage);
+    setDoneRecipesShow(doneRecipesInStorage);
   }, []);
+
+  useEffect(() => {
+    if (doneRecipesFilter.length > 0) {
+      setDoneRecipesShow(doneRecipesFilter);
+    }
+  }, [doneRecipesFilter]);
+  const shareRecipe = async ({ target }) => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), fiveSeconds);
+    await clipboardCopy(`http://localhost:3000${target.alt}`);
+  };
 
   return (
     <div>
       <Header />
       <div>
-        <button type="button" data-testid="filter-by-all-btn">All</button>
-        <button type="button" data-testid="filter-by-meal-btn">Meals</button>
-        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
-      </div>
-      <div>
-        {
-          doneRecipes.map((item, index) => (
-            <div key={ index }>
-              <img
-                style={ { width: '175px' } }
-                src={ item.image }
-                alt="Thumb da Receita"
-                data-testid={ `${index}-horizontal-image` }
-              />
-              <p
-                data-testid={ `${index}-horizontal-top-text` }
-              >
-                {item.category}
-              </p>
-              <p data-testid={ `${index}-horizontal-name` }>{item.name}</p>
-              <p data-testid={ `${index}-horizontal-done-date` }>{item.doneDate}</p>
-              <div data-testid={ `${index}-horizontal-share-btn` }>Compartilhar</div>
-              {
-                // obs: Acredito q o teste 44 esta quebrado. quando feito oq pede nao passa, mas se colocar o valor fixo 0, ele aprova. Com o valor de index dinamico Nao aprova.
+        <button
+          type="button"
+          data-testid="filter-by-all-btn"
+          onClick={ () => setDoneRecipesFilter(doneRecipes) }
+        >
+          All
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-meal-btn"
+          onClick={ () => setDoneRecipesFilter(doneRecipes
+            .filter((item) => item.type === 'meal')) }
+        >
+          Meals
+        </button>
+        <button
+          type="button"
+          data-testid="filter-by-drink-btn"
+          onClick={ () => setDoneRecipesFilter(doneRecipes
+            .filter((item) => item.type === 'drink')) }
+        >
+          Drinks
+        </button>
 
-                item.tags.map((it, ind) => (
-                  <span
-                    key={ ind }
-                    data-testid={ `${0}-${it}-horizontal-tag` }
+      </div>
+      {
+        doneRecipesShow.map((item, index) => {
+          if (item.type === 'meal') {
+            return (
+              <div key={ index }>
+
+                <button
+                  onClick={ ({ target }) => history.push(`/meals/${target.alt}`) }
+                  type="button"
+                >
+                  <img
+                    style={ { width: '175px' } }
+                    data-testid={ `${index}-horizontal-image` }
+                    src={ item.image }
+                    alt={ item.id }
+                  />
+                </button>
+                <button
+                  onClick={ ({ target }) => history.push(`/meals/${target.id}`) }
+                  type="button"
+                >
+                  <p
+                    id={ item.id }
+                    data-testid={ `${index}-horizontal-name` }
                   >
-                    {it}
-                  </span>
-
-                ))
-              }
-            </div>
-          ))
-        }
-      </div>
-      <div />
+                    {item.name}
+                  </p>
+                </button>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {`${item.nationality} - ${item.category}`}
+                </p>
+                <p
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  {item.doneDate}
+                </p>
+                <p
+                  data-testid={ `${0}-${item.tags[0]}-horizontal-tag` }
+                >
+                  {item.tags[0]}
+                </p>
+                <p
+                  data-testid={ `${0}-${item.tags[1]}-horizontal-tag` }
+                >
+                  {item.tags[1]}
+                </p>
+                <button
+                  type="button"
+                  onClick={ (e) => shareRecipe(e) }
+                >
+                  <img
+                    src={ shareIcon }
+                    alt={ `/meals/${item.id}` }
+                    data-testid={ `${index}-horizontal-share-btn` }
+                  />
+                </button>
+                {copied && (<span>Link copied!</span>)}
+              </div>
+            );
+          } if (item.type === 'drink') {
+            return (
+              <div key={ index }>
+                <button
+                  type="button"
+                  onClick={ ({ target }) => history.push(`/drinks/${target.alt}`) }
+                >
+                  <img
+                    id={ item.id }
+                    style={ { width: '175px' } }
+                    data-testid={ `${index}-horizontal-image` }
+                    src={ item.image }
+                    alt={ item.id }
+                  />
+                </button>
+                <button
+                  type="button"
+                  onClick={ ({ target }) => history.push(`/drinks/${target.id}`) }
+                >
+                  <p
+                    id={ item.id }
+                    data-testid={ `${index}-horizontal-name` }
+                  >
+                    {item.name}
+                  </p>
+                </button>
+                <p
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {item.alcoholicOrNot}
+                </p>
+                <p
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  {item.doneDate}
+                </p>
+                <button
+                  type="button"
+                  onClick={ (e) => shareRecipe(e) }
+                >
+                  <img
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    src={ shareIcon }
+                    alt={ `/drinks/${item.id}` }
+                  />
+                </button>
+                {copied && (<span>Link copied!</span>)}
+              </div>
+            );
+          }
+          return null;
+        })
+      }
     </div>
   );
 }
